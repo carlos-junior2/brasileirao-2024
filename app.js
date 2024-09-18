a97cba5a132bacf79a7b7dd595d63e190947b81b
Carlos Magno do Nascimento Junior <juninhonasc3@gmail.com>
2024-09-17 23:42:13 -0300

    Aula 4
    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(tabela2024);
 });
@@ -15,6 +17,16 @@ app.get('/:sigla', (requisicao, resposta) => {
     return;
     }
     resposta.status(200).send(time);
+});
+
+app.put('/:sigla', (req, res) => {
+    const s = req.params.sigla.toUpperCase();
+    const timeSelecionado = tabela2024.find(t => t.sigla === s);
+    const campos = Object.keys(req.body);
+    for(let campo of campos){
+        timeSelecionado[campo] = req.body[campo];
+    }
+    res.status(200).send(timeSelecionado);
 });
 
 app.listen(300, () => console.log("servidor rodando com sucesso"));
