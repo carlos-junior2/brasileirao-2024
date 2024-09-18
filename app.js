256ede89e591eb56167d3f57feb1af926d35b690
Carlos Magno do Nascimento Junior <juninhonasc3@gmail.com>
2024-09-17 23:10:59 -0300

    Aula 3
    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ import express from 'express';
 const app = express();
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send(tabela2024);
+    resposta.status(200).send(tabela2024);
 });
 
 app.get('/:sigla', (requisicao, resposta) => {
     var s = requisicao.params.sigla.toUpperCase();
     const time = tabela2024.find(infoTime => infoTime.sigla === s);
-    resposta.send(time);
-})
+    if (!time){
+        resposta.status(404).send('Este time não está na Serie A do Brasileirão');
+    return;
+    }
+    resposta.status(200).send(time);
+});
 
 app.listen(300, () => console.log("servidor rodando com sucesso"));
