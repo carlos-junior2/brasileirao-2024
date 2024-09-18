d9517c925e6ef06930681d5b5d89436689be1847
Carlos Magno do Nascimento Junior <juninhonasc3@gmail.com>
2024-09-17 22:40:30 -0300

    Aula 2
    criação do método get('/:sigla')

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.get('/', (requisicao, resposta) => {
     resposta.send(tabela2024);
+});
+
+app.get('/:sigla', (requisicao, resposta) => {
+    var s = requisicao.params.sigla.toUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === s);
+    resposta.send(time);
 })
 
 app.listen(300, () => console.log("servidor rodando com sucesso"));
